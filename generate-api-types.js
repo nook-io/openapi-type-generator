5b9d0347651c492adb036b1558e365e0682ed6a7
Johnny Deuss <johnny@nook.io>
2023-09-14 14:13:03 +0100

    Allow configuring of import path

--- generate-api-types.js
+++ generate-api-types.js
@@ -11,10 +11,17 @@ import Handlebars from "handlebars";
 
 const {values} = parseArgs({
   options: {
+    // The import root of the project.
+    'project-root': {
+      type: 'string',
+      default: './src/',
+    },
+    // the directory of the type directory, relative to the project root.
     'types-dir': {
       type: "string",
-      default: "src/types",
+      default: "types/",
     },
+    // The URL to download the openapi.json file from.
     'openapi-url': {
       type: "string",
       default: "http://localhost:8000/openapi.json",
@@ -22,6 +29,7 @@ const {values} = parseArgs({
   },
 });
 
+const projectRoot = values['project-root'];
 const typesDir = values['types-dir'];
 const openapiUrl = values['openapi-url'];
 
@@ -33,8 +41,8 @@ try {
   return
 }
 const typeFile = await openapiTS(response.data);
-const openapiGeneratePath = path.join(typesDir, 'openapi.generated.d.ts');
-fs.writeFileSync(openapiGeneratePath, typeFile);
+const openapiGeneratedPath = path.join(projectRoot, typesDir, 'openapi.generated.d.ts');
+fs.writeFileSync(openapiGeneratedPath, typeFile);
 const output = parser.parse(typeFile);
 
 const componentsNode = output.body.find(node => node.type === 'ExportNamedDeclaration' && node.declaration.id.name === 'components');
@@ -42,14 +50,15 @@ const componentProperties = componentsNode.declaration.body.body
 const schemasNode = componentProperties.find(node => node.type === 'TSPropertySignature' && node.key.name === 'schemas');
 const schemaProperties = schemasNode.typeAnnotation.typeAnnotation.members;
 const schemaNames = schemaProperties.map(node => node.key.name);
+const openapiPath = path.join(projectRoot, typesDir, 'openapi.generated');
 
 const template = Handlebars.compile(`\
-import {components} from 'types/openapi.generated';
+import {components} from '{{openapiPath}}';
 
-{{#each schemas}}
+{{#each schemaNames}}
 export type {{this}} = components["schemas"]["{{this}}"];
 {{/each}}
 `);
-const dataTypesFile = template({schemas: schemaNames});
-const schemasPath = path.join(typesDir, 'schemas.d.ts');
+const dataTypesFile = template({schemaNames, openapiPath});
+const schemasPath = path.join(projectRoot, typesDir, 'schemas.d.ts');
 fs.writeFileSync(schemasPath, dataTypesFile);
