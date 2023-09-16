a08d3c9adafe785a5fffffa973fc2bde622cf230
Johnny Deuss <johnny@nook.io>
2023-09-16 09:46:55 +0100

    Reexport enums

--- generate-api-types.js
+++ generate-api-types.js
@@ -2,63 +2,168 @@
 import { parseArgs } from "node:util";
 import fs from 'fs';
 import path from 'path';
-
-
-import axios from 'axios';
-import openapiTS from 'openapi-typescript';
+import { execSync } from 'child_process';
 import parser from '@typescript-eslint/parser';
-import Handlebars from "handlebars";
 
-const {values} = parseArgs({
-  options: {
-    // The import root of the project.
-    'project-root': {
-      type: 'string',
-      default: './src/',
-    },
-    // the directory of the type directory, relative to the project root.
-    'types-dir': {
-      type: "string",
-      default: "types/",
-    },
-    // The URL to download the openapi.json file from.
-    'openapi-url': {
-      type: "string",
-      default: "http://localhost:8000/openapi.json",
-    },
-  },
-});
+import openapiTS from 'openapi-typescript';
 
-const projectRoot = values['project-root'];
-const typesDir = values['types-dir'];
-const openapiUrl = values['openapi-url'];
 
-let response;
-try {
-  response = await axios.get(openapiUrl);
-} catch (e) {
-  console.log("Can't download openapi.json from localhost:8000. Please start the backend and try again.");
-  return
+function getArgs() {
+  const {values: args, tokens} = parseArgs({
+    options: {
+      // The import root of the project.
+      'project-root': {
+        type: 'string',
+        default: './src/',
+      },
+      // the directory of the type directory, relative to the project root.
+      'types-dir': {
+        type: "string",
+        default: "types/",
+      },
+      // The URL to download the OpenAPI file from.
+      'oas-url': {
+        type: "string",
+        default: ""
+      },
+      // The path to the OpenAPI file from.
+      'oas-path': {
+        type: "string",
+        default: ""
+      },
+      // A command to an executable that outputs the OpenAPI file.
+      'oas-command': {
+        type: "string",
+        default: ""
+      },
+    },
+    tokens: true
+  });
+
+  if (!args['oas-url'] && !args['oas-path'] && !args['oas-command']) {
+    console.log('Must provide either --oas-url, --oas-path or --oas-command');
+    process.exit(0);
+  }
+
+  const importOrder = tokens
+    .filter(token => token.name.startsWith('oas'))
+    .sort((a, b) => a.index - b.index)
+    .map(token => token.name);;
+
+  return {args, importOrder};
 }
-const typeFile = await openapiTS(response.data);
-const openapiGeneratedPath = path.join(projectRoot, typesDir, 'openapi.generated.d.ts');
-fs.writeFileSync(openapiGeneratedPath, typeFile);
-const output = parser.parse(typeFile);
 
-const componentsNode = output.body.find(node => node.type === 'ExportNamedDeclaration' && node.declaration.id.name === 'components');
-const componentProperties = componentsNode.declaration.body.body
-const schemasNode = componentProperties.find(node => node.type === 'TSPropertySignature' && node.key.name === 'schemas');
-const schemaProperties = schemasNode.typeAnnotation.typeAnnotation.members;
-const schemaNames = schemaProperties.map(node => node.key.name);
-const openapiPath = path.join(projectRoot, typesDir, 'openapi.generated');
+/**
+ * Load the OpenAPI schema from the given source.
+ */
+async function loadOpenAPISchema(args, importOrder) {
+  let openAPIFile;
+  for (const importKey of importOrder) {
+    switch (importKey) {
+      case 'oas-path':
+        openAPIFile = fs.readFileSync(args['oas-path']);
+        return JSON.parse(openAPIFile);
+      case 'oas-command':
+        openAPIFile =  execSync(args['oas-command']);
+        return JSON.parse(openAPIFile);
+      case 'oas-url':
+        const response = await fetch(args['oas-url']);
+        return await response.json();
+    }
+  }
+  throw new Error('Could not load OpenAPI file');
+}
 
-const template = Handlebars.compile(`\
-import {components} from '{{openapiPath}}';
+/**
+ * Convert an enum schema into a string representation of a
+ * TypeScript enum.
+ */
+function formatEnum(enumSchema) {
+  const enumName = enumSchema.title;
+  const commentString = enumSchema.description ? `/**\n * ${enumSchema.description}\n */\n` : '';
+  const enumValues = enumSchema.enum
+    .map((value) => `  ${JSON.stringify(value)} = ${JSON.stringify(value)}`)
+    .join(',\n');
+  return `\
+${commentString}\
+enum ${enumName.replace(/[^\w\d]/, '')} {
+${enumValues}
+}`;
+}
 
-{{#each schemaNames}}
-export type {{this}} = components["schemas"]["{{this}}"];
-{{/each}}
-`);
-const dataTypesFile = template({schemaNames, openapiPath});
-const schemasPath = path.join(projectRoot, typesDir, 'schemas.d.ts');
-fs.writeFileSync(schemasPath, dataTypesFile);
+/**
+ * Transform union enums into a single enum when they have their own
+ * schema, i.e. are referenced through a $ref, not inlined. Used with
+ * openapiTS.
+ */
+function transform(schemaObject, metadata) {
+  if (!('enum' in schemaObject)) {
+    return '';
+  }
+  const enumName = schemaObject.title;
+  if (!enumName) {
+    return '';
+  }
+  const schemaName = metadata.path.split('/').at(-1);
+  if (schemaName !== enumName) {
+    return '';
+  }
+  if (!enumLookup[enumName]) {
+    enumLookup[enumName] = schemaObject;
+  }
+  return enumName;
+}
+
+/**
+ * Generate a re-exporter file for the generated types. This file
+ * imports the components file from the generated OpenAPI types file
+ * and re-exports all the schemas from it, as well as re-exporting all
+ * enums. The purpose of this file is to flatten the generated types, so
+ * you can use a type like `const x: MySchema` instead of
+ * `const x: components['schemas']['MySchema']`.
+ */
+function generateReExporterFile(typeFile, typesDir, enumLookup) {
+  const output = parser.parse(typeFile);
+  const componentsNode = output.body.find(node => node.type === 'ExportNamedDeclaration' && node.declaration.id.name === 'components');
+  const componentProperties = componentsNode.declaration.body.body
+  const schemasNode = componentProperties.find(node => node.type === 'TSPropertySignature' && node.key.name === 'schemas');
+  const schemaProperties = schemasNode.typeAnnotation.typeAnnotation.members;
+  const openAPIPath = path.join(typesDir, 'openapi');
+  
+  let reExporterLines = [`import { components } from '${openAPIPath}';\n`];
+  if (Object.keys(enumLookup).length > 0) {
+    reExporterLines.push('export {');
+    for (const enumName in enumLookup) {
+      reExporterLines.push(`  ${enumName},`);
+    }
+    reExporterLines.push(`} from '${openAPIPath}';`);
+  }
+  reExporterLines.push('');
+
+  reExporterLines = reExporterLines.concat(schemaProperties.filter(schemaProperty => !(schemaProperty.key.name in enumLookup)).map(schemaProperty => {
+    const schemaName = schemaProperty.key.name;
+    const isEnum = schemaProperty.key.type === 'Identifier' && schemaName in enumLookup;
+    return `export ${isEnum ? "const" : "type"} ${schemaName} = components['schemas']['${schemaName}}'];`;
+  }));
+  reExporterLines.push('');
+  return reExporterLines.join('\n');
+}
+
+const {args, importOrder} = getArgs();
+const openAPISchema = await loadOpenAPISchema(args, importOrder);
+const enumLookup = {};
+
+let typeFile;
+try {
+  typeFile = await openapiTS(openAPISchema, {transform});
+} catch (e) {
+  console.log(e);
+  process.exit(0);
+}
+const enumDefs = Object.values(enumLookup).map((enumSchema) => formatEnum(enumSchema)).join('\n');
+typeFile += '\n' + enumDefs + '\n';
+const openAPIGeneratedPath = path.join(args['project-root'], args['types-dir'], 'openapi.d.ts');
+fs.writeFileSync(openAPIGeneratedPath, typeFile);
+const reExporterFile = generateReExporterFile(typeFile, args['types-dir'], enumLookup);
+const schemasPath = path.join(args['project-root'], args['types-dir'], 'schemas.d.ts');
+fs.writeFileSync(schemasPath, reExporterFile);
